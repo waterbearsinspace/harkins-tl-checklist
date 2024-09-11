c16e213c94523f2d664d2217d8c6793d79606a34
waterbearsinspace <nicolasviste@gmail.com>
2024-09-10 18:22:34 -0700

    restructure files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './routes/home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import './App.css'
 
-function App() {
+function Home() {
 
   return (
     <>
@@ -9,4 +8,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
